c07f272e3b2996ca1aa8443a9b0596099a763d4f
iliev04 <166809343+iliev04@users.noreply.github.com>
2025-11-13 20:42:36 +0200

    Added Main Page

--- app/src/main/java/com/example/mobileapps2025_2301321062/ui/theme/Color.kt
+++ app/src/main/java/com/example/mobileapps2025_2301321062/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.mobileapps2025_2301321062.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue80 = Color(0xFF64B5F6)
+val DarkBlue80 = Color(0xFF90CAF9)
+val LightBlue80 = Color(0xFFE3F2FD)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue40 = Color(0xFF1976D2)
+val DarkBlue40 = Color(0xFF0D47A1)
+val LightBlue40 = Color(0xFFBBDEFB)
